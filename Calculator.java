a64882a533df8bc29cb52bb4479ca48424fa64b5
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2024-02-27 15:22:15 +0300

    created calculator

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+
+
+public static void main(String[] args) throws ArithmeticException {
+
+    double purchaseAmount = -1000;
+    int discountAmount = 10;
+    System.out.println(calculatingDiscount(purchaseAmount,discountAmount));
+}
+// Метод возвращает сумму покупки со скидкой
+public static double calculatingDiscount(double purchaseAmount, int discountAmount) throws ArithmeticException {
+    if (purchaseAmount <= 0){
+        throw new ArithmeticException("Отрицательная цена");}
+    if (discountAmount < 0){
+        throw new ArithmeticException("Отрицательная скидка");
+    }
+    else if (discountAmount > 100){
+        throw new ArithmeticException("Скидка больше 100%");
+    }
+    return purchaseAmount / 100 * (100 - discountAmount);
+}
+
+
+
+class ArithmeticException extends Exception {
+    public ArithmeticException(String message) {super(message);
+    }
+}
